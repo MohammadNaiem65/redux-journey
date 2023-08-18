bfc9d5bb245d96e91105cb22833009ab8c1355a4
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2023-08-18 15:12:48 +0600

    used connect to setup redux

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,18 +1,7 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { connect } from 'react-redux';
 import { decrement, increment } from '../redux/Counter/ActionCreators';
 
-export default function Counter() {
-	const count = useSelector((state) => state.value);
-	const dispatch = useDispatch();
-
-	const incrementHandler = () => {
-		dispatch(increment());
-	};
-
-	const decrementHandler = () => {
-		dispatch(decrement());
-	};
-
+function Counter({ count, incrementHandler, decrementHandler }) {
 	return (
 		<div className='w-1/3 p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow'>
 			<div className='text-2xl font-semibold text-gray-500'>{count}</div>
@@ -31,3 +20,18 @@ export default function Counter() {
 		</div>
 	);
 }
+
+function mapStateToProps(state) {
+	return {
+		count: state.value,
+	};
+}
+
+function mapDispatchToProps(dispatch) {
+	return {
+		incrementHandler: () => dispatch(increment()),
+		decrementHandler: () => dispatch(decrement()),
+	};
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
